ad45686af6e36cb9151298681612b19cd18aa156
lakansantos <rizesuoracle25@gmail.com>
2026-01-02 13:42:44 +0800

    Fixed no data handling for saved anine

--- app/modules/Anime/AnimeSaved/AnimeSaved.tsx
+++ app/modules/Anime/AnimeSaved/AnimeSaved.tsx
@@ -14,65 +14,68 @@ const AnimeSaved = () => {
   const isDataEmpty = !savedData || savedData.length === 0;
   const isRecEmpty = !savedRecommendations || savedRecommendations.length === 0;
 
+  const isBothDataEmpty = isDataEmpty && isRecEmpty;
+
   return (
     <section className="w-full">
       <AnimeSavedBanner />
-
-      {/* Saved Anime */}
-      {isDataEmpty ? (
-        <div className="mt-3">
-          <AnimeNoData />
-        </div>
+      {isBothDataEmpty ? (
+        <AnimeNoData />
       ) : (
-        <div className="mt-6">
-          <div className="flex flex-col mb-6 max-w-full">
-            <div
-              className={cx(
-                "grid grid-cols-[repeat(auto-fit,minmax(400px,1fr))] gap-4",
-                {
-                  "lg:grid-cols-[repeat(auto-fit,minmax(400px,400px))]":
-                    savedData.length <= 3,
-                }
-              )}
-            >
-              {savedData.map((item, index) => (
-                <AnimeSavedCard
-                  item={item}
-                  key={`${index}-${item.id}`}
-                  onRemove={remove}
-                />
-              ))}
+        <>
+          {/* Saved Anime */}
+          {!isDataEmpty && (
+            <div className="mt-6">
+              <div className="flex flex-col mb-6 max-w-full">
+                <div
+                  className={cx(
+                    "grid grid-cols-[repeat(auto-fit,minmax(400px,1fr))] gap-4",
+                    {
+                      "lg:grid-cols-[repeat(auto-fit,minmax(400px,400px))]":
+                        savedData.length <= 3,
+                    }
+                  )}
+                >
+                  {savedData.map((item, index) => (
+                    <AnimeSavedCard
+                      item={item}
+                      key={`${index}-${item.id}`}
+                      onRemove={remove}
+                    />
+                  ))}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      )}
-      {/* Saved Recommendations */}
-      {!isRecEmpty && (
-        <div className="mt-10">
-          <h2 className="text-white text-xl font-semibold mb-4">
-            Saved Recommendations
-          </h2>
+          )}
+          {/* Saved Recommendations */}
+          {!isRecEmpty && (
+            <div className="mt-10">
+              <h2 className="text-white text-xl font-semibold mb-4">
+                Saved Recommendations
+              </h2>
 
-          <div className="flex flex-col mb-6 max-w-full">
-            <div
-              className={cx(
-                "grid grid-cols-[repeat(auto-fit,minmax(400px,1fr))] gap-4",
-                {
-                  "lg:grid-cols-[repeat(auto-fit,minmax(400px,400px))]":
-                    savedRecommendations.length <= 3,
-                }
-              )}
-            >
-              {savedRecommendations.map((item, index) => (
-                <AnimeSavedRecommendationCard
-                  key={`${index}-${item.id}`}
-                  item={item}
-                  onRemove={onRemoveSavedRecommendations}
-                />
-              ))}
+              <div className="flex flex-col mb-6 max-w-full">
+                <div
+                  className={cx(
+                    "grid grid-cols-[repeat(auto-fit,minmax(400px,1fr))] gap-4",
+                    {
+                      "lg:grid-cols-[repeat(auto-fit,minmax(400px,400px))]":
+                        savedRecommendations.length <= 3,
+                    }
+                  )}
+                >
+                  {savedRecommendations.map((item, index) => (
+                    <AnimeSavedRecommendationCard
+                      key={`${index}-${item.id}`}
+                      item={item}
+                      onRemove={onRemoveSavedRecommendations}
+                    />
+                  ))}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          )}
+        </>
       )}
     </section>
   );
