97615c4236145d471bd70bd9a11310de6b132b92
lakansantos <rizesuoracle25@gmail.com>
2026-01-04 18:52:44 +0800

    Fixed banner skeleton blinking if no cover image

--- app/modules/Details/DetailsBanner.tsx
+++ app/modules/Details/DetailsBanner.tsx
@@ -15,37 +15,38 @@ const DetailsBanner = () => {
 
   return (
     <div className="relative w-full overflow-hidden h-[25vh] md:h-[35vh] border-b-2 border-white/60">
-      {/* Banner Skeleton */}
-      <div
-        className={classNames(
-          "absolute inset-0 bg-gray-800 rounded-md animate-pulse z-9",
-          {
-            "opacity-100": !load,
-            hidden: load,
-          }
-        )}
-      />
-
       {coverImage ? (
-        <div
-          className={classNames("", {
-            "opacity-100": load,
-            "opacity-0": !load,
-          })}
-        >
-          {/* Background image */}
-          <Image
-            src={coverImage.original}
-            alt="Banner"
-            fill
-            priority
-            sizes="100vw"
-            onLoad={() => setLoad(true)}
-            className="object-cover object-center scale-105 saturate-125 contrast-110"
+        <>
+          <div
+            className={classNames("", {
+              "opacity-100": load,
+              "opacity-0": !load,
+            })}
+          >
+            {/* Background image */}
+            <Image
+              src={coverImage.original}
+              alt="Banner"
+              fill
+              priority
+              sizes="100vw"
+              onLoad={() => setLoad(true)}
+              className="object-cover object-center scale-105 saturate-125 contrast-110"
+            />
+            {/* Color overlay (anime glow) */}
+            <div className="absolute inset-0 bg-gradient-to-tr from-indigo-900/20 via-transparent to-orange-500/20" />
+          </div>
+          {/* Banner Skeleton */}
+          <div
+            className={classNames(
+              "absolute inset-0 bg-gray-800 rounded-md animate-pulse z-9",
+              {
+                "opacity-100": !load,
+                hidden: load,
+              }
+            )}
           />
-          {/* Color overlay (anime glow) */}
-          <div className="absolute inset-0 bg-gradient-to-tr from-indigo-900/20 via-transparent to-orange-500/20" />
-        </div>
+        </>
       ) : (
         <div className="self-center h-full flex justify-center items-center gap-2 flex-col z-20">
           <PiImageBroken className="text-5xl" />
